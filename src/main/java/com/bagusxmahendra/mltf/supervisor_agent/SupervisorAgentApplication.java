da4f792fee452983522027f9bfe24d5f1e894c93
bagus <bagusmwicaksono@gmail.com>
2026-08-24 09:17:08 +0000

    Initialize

---
+++ src/main/java/com/bagusxmahendra/mltf/supervisor_agent/SupervisorAgentApplication.java
@@ -0,0 +1,13 @@
+package com.bagusxmahendra.mltf.supervisor_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupervisorAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupervisorAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bagusxmahendra/mltf/supervisor_agent/SupervisorAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bagusxmahendra.mltf.supervisor_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupervisorAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
